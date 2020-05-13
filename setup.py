bc6a06283ac3c96a8a8dfdffc9ae1e561ce1ce43
Kevin <ksmith@hmc.edu>
2020-05-13 01:01:49 -0700

    Updated version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='PyMoments',
-    version='1.0.0',
+    version='1.0.1',
     description='Unbiased estimators for multivariate statistical moments',
     url='https://github.com/KevinDalySmith/PyMoments',
     author='Kevin D. Smith',
